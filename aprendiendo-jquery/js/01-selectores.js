c809a4e1530966dba9ded9503010592c2dd65182
Arturo Romero <aromero@aeonlinesolutions.com>
2021-02-05 00:12:11 -0500

    se agrega git ignore y jquery

---
+++ aprendiendo-jquery/js/01-selectores.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    
+    // selector de id
+    $("#amarillo").css("background", "yellow")
+                  .css("color", "black");
+
+    $("#rojo").css("background", "red")
+              .css("color", "white");
+
+    $("#azul").css("background", "blue")
+              .css("color", "white");
+
+})
