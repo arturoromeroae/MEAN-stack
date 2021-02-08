dcafb6b39b94ae57ce2f79b15cfab9debc009acc
Arturo Romero <aromero@aeonlinesolutions.com>
2021-02-07 22:37:45 -0500

    se agregan ficheros AJAX

--- aprendiendo-jquery/js/04-efectos.js
+++ aprendiendo-jquery/js/04-efectos.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
+    var caja = $('#caja');
+
     $('#show').hide();
 
     $('#show').click(function () {
@@ -8,7 +10,7 @@ $(document).ready(function () {
         // $('#caja').show('slow');  mostrar
         // $('#caja').fadeIn('slow');  mostrar con efecto
         // $('#caja').slideDown('slow');  mostrar con efecto
-        $('#caja').fadeTo('slow', 1);  // mostrar 1
+        caja.fadeTo('slow', 1);  // mostrar 1
     });
 
     $('#hidden').click(function () {
@@ -16,11 +18,38 @@ $(document).ready(function () {
         $('#show').show();
         // $('#caja').hide('slow');  ocultar
         // $('#caja').fadeOut('slow');  ocultar con efecto
-        // $('#caja').slideUp('slow');  ocultar con efecto
-        $('#caja').fadeTo('slow', 0); // ocultar 0
+        //caja.fadeTo('slow', 0); // ocultar 0
+        $('#caja').slideUp('slow', function() {
+            console.log('cartel ocultado'); // se ejecuta cuando termina la animacion
+        });  // ocultar con efecto
     });
 
     $('#allinone').click(function () {
-        $('#caja').slideToggle('fast');
+        caja.slideToggle('fast');
+    });
+
+    $('#animar').click(function () {
+        caja.animate({
+                        marginLeft: '500px',
+                        fontSize: '40px',
+                        height: '100px'
+                        }, 'slow')
+            .animate({
+                borderRadius: '900px',
+                marginTop: '150px'
+            }, 'slow')
+            .animate({
+                borderRadius: '0px',
+                marginLeft: '0px'
+            }, 'slow')
+            .animate({
+                marginTop: '0px',
+                borderRadius: '100px'
+            }, 'slow')
+            .animate({
+                marginLeft: '500px',
+                fontSize: '40px',
+                height: '100px'
+                }, 'slow')
     });
 });
