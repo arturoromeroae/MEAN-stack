b792d01329f7c23ed28193bc8ee9e3448684eda6
Arturo Romero <aromero@aeonlinesolutions.com>
2021-02-09 22:42:40 -0500

    se agregan mas efectos

--- aprendiendo-jquery/js/06-interface.js
+++ aprendiendo-jquery/js/06-interface.js
@@ -1,3 +1,51 @@
 $(document).ready(function () {
-    
+
+    // mover elemento por la pagina
+    $('.elemento').draggable();
+
+    // redimensionar
+    $('.elemento').resizable();
+
+    // seleccionar y ordenar elementos
+    // $('.lista-seleccionable').selectable(); // selecciona
+    $('.lista-seleccionable').sortable({
+        update: function(event, ui) {
+            console.log('ha cambiado la lista');
+        }
+    }); // ordena
+
+    // drop
+    $('#elemento-movido').draggable();
+    $('#area').droppable({
+        drop: function () {
+            console.log('soltaste algo dentro del area')
+        }
+    });
+
+    // efectos
+    $('#mostrar').click(function () { 
+        // $('.caja-efectos').fadeToggle();
+        // $('.caja-efectos').toggle('explode');
+        // $('.caja-efectos').toggle('blind');
+        // $('.caja-efectos').toggle('slide');
+        // $('.caja-efectos').toggle('drop');
+        // $('.caja-efectos').toggle('fold');
+        // $('.caja-efectos').toggle('puff');
+        // $('.caja-efectos').toggle('scale');
+        $('.caja-efectos').toggle('shake', /* 'slow'*/ 4000);
+    });
+
+    // tooltip
+    $(document).tooltip();
+
+    // dialog
+    $('#lanzar-popup').click(function () {
+        $('#popup').dialog();
+    });
+
+    // datepicker
+    $('#calendario').datepicker();
+
+    // tabs
+    $('#pestanas').tabs();
 });
