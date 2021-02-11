1348cfc46434af0bc4f6526291d05a9c72993ac7
Arturo Romero <aromero@aeonlinesolutions.com>
2021-02-11 01:00:26 -0500

    se mejora el project

---
+++ proyecto-js/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    // slider
+    $('.bxslider').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 1500,
+        responsive: true
+    });
+});
