a989a7bedfb7b718b4f7f8c2d8f98b2461146b8c
Arturo Romero <aromero@aeonlinesolutions.com>
2021-02-21 00:01:25 -0500

    se crean nuevos componentes

---
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'videojuego',
+    templateUrl: './videojuego.component.html'
+})
+
+export class VideojuegoComponent {
+    public titulo: string;
+    public listado: string;
+
+    constructor(){
+        this.titulo = 'Componente de Videojuegos';
+        this.listado = 'Listado de los juegos mas populares: '
+        console.log('se ha cargado el componente videojuego');
+    }
+}
