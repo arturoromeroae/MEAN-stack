b900cd084267cd7faabe3a7dbc4fcfdd2d9a9962
unknown <aromero@aeonlinesolutions.com>
2021-03-14 17:50:48 -0500

    se continua el curso

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -10,12 +10,15 @@ export class ZapatillasComponent implements OnInit {
     public zapatillas: Array<Zapatilla>;
     public marcas: String[];
     public color: string;
+    public mi_marca: string;
 
     constructor(){
+        this.mi_marca = 'fila';
         this.color = 'yellow';
         this.marcas = new Array();
         this.zapatillas = [
             new Zapatilla('Steven', 100, 'Reebok', 'blancas', true),
+            new Zapatilla('Marriot', 150, 'Puma', 'blancas', true),
             new Zapatilla('Stive', 100, 'Reebok', 'negras', false),
             new Zapatilla('Mike', 80, 'Puma', 'grises', true),
             new Zapatilla('Pedro', 97, 'Nike', 'blancas', true)
@@ -35,5 +38,26 @@ export class ZapatillasComponent implements OnInit {
             }
         });
         console.log(this.marcas);
+    }
+
+    getMarca(){
+        alert(this.mi_marca);
+    }
+
+    addMarca(){
+        this.marcas.push(this.mi_marca);
+    }
+
+    borrarMarca(index: any){
+        // delete this.marcas[index];
+        this.marcas.splice(index, 1);
+    }
+
+    onBlur(){
+        console.log("has salido del input");
+    }
+
+    mostrarPalabra(){
+        alert(this.mi_marca);
     }
 }
